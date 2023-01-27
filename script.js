267d6a6028a29271317bf1c4038e9dc65bafe732
tj topicz <tjtopicz@gmail.com>
2023-01-27 09:00:10 -0600

    wrote base js code for basic checkerboard

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-// Your JS goes here
+// * Each tile should be a `div`
+// * Each tile's width is `11.1%`
+// * Set each tile's `float` property to `left`
+// * Each tile's paddingBottom is `11.1%`
+
+//for loop to create 81 divvs/blocks (checkers is 9x9 = 81):
+// for (let x =0; x < 81; x++){
+//     let block = document.createElement("div");
+    
+// };
+for (let x = 0; x < 81; x++) {
+    let tile = document.createElement("div");
+    tile.style.float = "left";
+    tile.style.minWidth = "11.1%";
+    tile.style.paddingBottom = "11.1%";
+
+    if (x % 2 === 0) {
+        tile.classList.add('even');
+        tile.style.backgroundColor = "black";
+    } else {
+        tile.classList.add('odd');
+        tile.style.backgroundColor = "red";
+    }
+    document.body.appendChild(tile);
+};
